a352ccc225cee4ac71b110c55228ed810cb3c061
pedro <pedro@rosello.dev>
2024-09-25 11:52:59 +0100

    Section 20

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,19 +1,7 @@
-import { app } from "./app";
-import mongoose from "mongoose";
 import { natsWrapper } from "./nats-wrapper";
-import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
-import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
-import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const main = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
-  }
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
@@ -40,15 +28,7 @@ const main = async () => {
   process.on("SIGINT", () => natsWrapper.client.close());
   process.on("SIGTERM", () => natsWrapper.client.close());
 
-  new TicketCreatedListener(natsWrapper.client).listen();
-  new TicketUpdatedListener(natsWrapper.client).listen();
-  new ExpirationCompleteListener(natsWrapper.client).listen();
-
-  await mongoose.connect(process.env.MONGO_URI);
-
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
-  });
+  new OrderCreatedListener(natsWrapper.client).listen();
 };
 
 main().catch((error) => {
